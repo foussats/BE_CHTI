c240722cf7c7f716240ad5560c0142d9a200d24c
foussats <foussats@etud.insa-toulouse.fr>
2020-05-20 18:02:55 +0200

    Codage du signal sonore à émettre pour l'objectif 3

--- Src/Carre.s
+++ Src/Carre.s
@@ -16,39 +16,47 @@ TIM3_CCR3	equ		0x4000043C	; adresse registre PWM
 GPIOB_BSRF	equ		0x40010C10	; Bit Set/Reset register
 	
 timer_callback proc
-
+		;on met l'adresse de la variable etat dans r2
 		ldr		r2, =etat
+		;on charge la valeur de la variable etat.position dans r0
 		ldr 	r0, [r2, #E_POS]
+		;on charge la valeur de la variable etat.taille dans r0
 		ldr 	r1, [r2, #E_TAI]
+		;comparaison de la position et de la taille
 		cmp 	r0,r1
 		blt		cond
 		
-	; silence ;
+	; si po>taille alors emission de silence ;
 		push	{r4,r5}
+		;on met la valeur 3 dans r4
 		mov 	r4, #3
+		;on load l'adresse de TIM3_CRR3 dans r5
 		ldr 	r5, =TIM3_CCR3
+		;on charge la valeur 3 dans la variable TIM3_CRR3
 		str 	r4,[r5]
 		pop		{r4,r5}
 		bx		lr
 	
-	; bruit ;
-		; dans r3, valeur du son
+	; si pos<taille alors émission de bruit ;
+		; dans r3, valeur de la variable etat.son
 cond	ldr		r3,[r2, #E_SON]
 		ldrsh 	r3,[r3,r0, LSL #1]
 		push 	{r4,r5,r6,r7}
-		;+b
+		;ajout composante continue
 		add		r3,#32768
 		
 		;1/65536 fait 0,0000153 
 		ldr		r6,[r2, #E_RES]
 		mov		r7,#65536
+		;on multiplie la résolution par la valeur de la variable etat.son 
+		;puis on divise par 65536 pour mettre à l'achelle
 		mul 	r3, r6
 		udiv 	r3, r7
 		
-		
+		;on charge la valeur de l'échantillon mise à l'échelle dans TIM3_CRR3
 		ldr 	r7, =TIM3_CCR3
 		str 	r3, [r7]
-		
+		;on incrémente la position pour passer à l'échantillon suivant
 		add 	r0, #1
 		str		r0, [r2, #E_POS]
 		
